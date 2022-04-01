0633e5945d805f0dbf64f0571f90fcf5324fc73a
Sadie Manicom <sadienmanicom@gmail.com>
2022-03-31 18:22:03 -0700

    added a (not good) pw generator and general update

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
+let characters = 0;
+let password = "";
+const options = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?";
 const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
@@ -13,3 +16,18 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className);
 });
+
+function randomPassCreator(characters)
+{
+    characters = characters + 2;
+    const newPassword = Math.random().toString(36).substring(2,characters);
+    return newPassword;
+}
+
+function pwGenerator()
+{
+    var x = document.getElementById("numCharacters").value;
+    var inputInt = parseInt(x);
+    password = randomPassCreator(inputInt);
+    document.getElementById("practice").innerHTML = password;
+}
