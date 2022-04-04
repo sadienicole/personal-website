64ff1afcac65a90bfddfe268f408991493136aa8
Sadie <sadienmanicom@gmail.com>
2022-04-04 07:40:05 -0700

    minor visuals and inclusion of task bar

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ switcher.addEventListener('click', function() {
 
     var className = document.body.className;
     if(className == "light-theme") {
-        this.textContent = "Dark";
+        this.textContent = "green";
     }
     else {
-        this.textContent = "Light";
+        this.textContent = "blue";
     }
 
     console.log('current class name: ' + className);
